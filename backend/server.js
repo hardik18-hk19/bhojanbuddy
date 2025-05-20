ee1f8282dfb82992bdab6f9387acb4f0fd8467a7
Manyathaaa <mailtomanyatha@gmail.com>
2025-05-20 14:20:07 +0530

    database added and connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/dbConnect.js";
 
 dotenv.config();
 
@@ -10,3 +11,5 @@ const PORT = process.env.PORT || 1011;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+connectDB()
