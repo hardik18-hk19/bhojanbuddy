6ac31bfbfdc92826fc20aaef8155c228e1a841e7
Manyathaaa <mailtomanyatha@gmail.com>
2025-05-20 14:55:07 +0530

    route of auth done

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/dbConnect.js";
 
+import authRoutes from "./routes/AuthRoute.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 1011;
+app.use("/api", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-connectDB()
+connectDB();
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
